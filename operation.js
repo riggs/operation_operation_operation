8932b71ebbb716ac39dbc4b1851a76bddf293d5d
Benjamin Riggs <deisum@gmail.com>
2016-06-30 03:12:02 -0500

    Added default message logging for debug, config for XLMS.

--- operation.js
+++ operation.js
@@ -2,3 +2,13 @@
  * Created by riggs on 6/30/16.
  */
 "use strict";
+
+
+
+window.addEventListener('load', () => {
+  
+  window.addEventListener('message', (message) => {
+    console.log(...message);
+  })
+  
+});
